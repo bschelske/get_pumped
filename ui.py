ed36c4c9a5ef0aa40949d39051e5fae2b5f8e5bd
bschelske <103594627+bschelske@users.noreply.github.com>
2024-08-02 11:37:41 -0500

    ui initialization window, pump control window

--- ui.py
+++ ui.py
@@ -1,21 +1,23 @@
 import tkinter as tk
-from tkinter import filedialog
+
+from classes import SyringePump, MultifrequencyBoard
 
 
 class InitializationInterface:
     def __init__(self, master):
         self.master = master
-
+        # Default COM values
         self.outlet_COM = tk.StringVar(value='COM1')
         self.cell_COM = tk.StringVar(value='COM1')
         self.buffer_COM = tk.StringVar(value='COM1')
         self.waste_COM = tk.StringVar(value='COM1')
         self.mf_COM = tk.StringVar(value='COM1')
-
+        # Default address values
         self.outlet_ADDR = tk.StringVar(value='0')
         self.cell_ADDR = tk.StringVar(value='1')
         self.buffer_ADDR = tk.StringVar(value='2')
         self.waste_ADDR = tk.StringVar(value='3')
+
         self.create_toolbar()
         self.create_initialization_window()
 
@@ -87,6 +89,10 @@ class InitializationInterface:
         self.entry_value = tk.Entry(self.master, textvariable=self.mf_COM, width=COM_width, justify='center')
         self.entry_value.grid(row=row, column=col + 1, padx=5, pady=5)
 
+        # Connect button
+        self.connect_button = tk.Button(self.master, text="Test", command=self.press_connect, width=7)
+        self.connect_button.grid(row=row+1, column=1, padx=1, pady=10)
+
         # OK button
         self.OK_button = tk.Button(self.master, text="OK", command=self.press_ok, width=7)
         self.OK_button.grid(row=row+1, column=2, padx=1, pady=10)
@@ -95,28 +101,42 @@ class InitializationInterface:
         self.quit_button = tk.Button(self.master, text="Quit", command=self.quit_ui, width=7)
         self.quit_button.grid(row=row+1, column=3, padx=1, pady=10)
 
-
-
     def quit_ui(self):
         self.master.destroy()
         quit(2)
 
     def press_ok(self):
+        self.press_connect()
         self.master.destroy()
         new_window = tk.Tk()
         new_window.title("Get Pumped!")
         PumpControlUserInterface(new_window, self)
 
+    def press_connect(self):
+        # Create Pump instances
+        self.outlet_pump = SyringePump(self.outlet_ADDR.get(), self.outlet_COM.get(), 'Outlet pump')
+        self.cell_pump = SyringePump(self.cell_ADDR.get(), self.cell_COM.get(), 'Cell pump')
+        self.buffer_pump = SyringePump(self.buffer_ADDR.get(), self.buffer_COM.get(), 'Buffer pump')
+        self.waste_pump = SyringePump(self.waste_ADDR.get(), self.waste_COM.get(), 'Waste pump')
+
+        # Create MF instance
+        self.mf_board = MultifrequencyBoard(self.mf_COM.get())
+
 
 class PumpControlUserInterface:
-    def __init__(self, master, init_settings):
-        self.init_settings = init_settings
+    def __init__(self, master, InitializationInstance):
+        self.device = InitializationInstance
         self.master = master
-        self.placeholder_value = tk.IntVar(value=10)
 
+        self.set_ui_default_values()
+        self.set_initial_hardware_values()
+        self.create_toolbar()
+        self.create_pump_controls()
+
+    def set_ui_default_values(self):
         # Pump default flow rates
+        #   Add default values to UI
         self.inlet_flow_rate = tk.StringVar(value="1200 um")
-
         self.buffer_rate = self.inlet_flow_rate
         self.cell_rate = self.inlet_flow_rate
         self.waste_rate = self.inlet_flow_rate
@@ -134,8 +154,29 @@ class PumpControlUserInterface:
         self.mf_amp = tk.StringVar(value='28')
         self.mf_frequency = tk.StringVar(value='90')
 
-        self.create_toolbar()
-        self.create_pump_controls()
+    def set_initial_hardware_values(self):
+        # Inlet Pump flow rates
+        self.device.buffer_pump.input('irate ' + self.buffer_rate.get())
+        self.device.cell_pump.input('irate ' + self.cell_rate.get())
+        self.device.waste_pump.input('wrate ' + self.waste_rate.get())
+
+        # Outlet pump flow rates
+        self.device.outlet_pump.input('wrate ' + self.outlet_capture.get())
+
+        # Pump default target volumes
+        # Clear volumes
+        self.device.buffer_pump.input('cvolume')
+        self.device.cell_pump.input('cvolume')
+        self.device.waste_pump.input('cvolume')
+
+        # Set target values
+        self.device.buffer_pump.input('tvolume ' + self.buffer_volume())
+        self.device.cell_pump.input('tvolume ' + self.cell_volume.get())
+        self.device.waste_pump.input('tvolume ' + self.waste_volume.get())
+
+        # Set MF Board Parameters
+        self.device.mf_board.amplitude = self.mf_amp
+        self.device.mf_board.frequency = self.mf_frequency
 
     def create_toolbar(self):
         menu = tk.Menu(self.master)
@@ -240,7 +281,6 @@ class PumpControlUserInterface:
         self.master.destroy()
         # new_window = tk.Tk()
         # new_window.title("New Window")
-
 
 
 def create_ui():
